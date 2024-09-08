c101b907d37431dc9e90e59e4e497bbec97952fa
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2024-09-08 13:44:11 +0530

    Added hello function

--- Sources/MyLibraryProject/MyLibraryProject.swift
+++ Sources/MyLibraryProject/MyLibraryProject.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class Testing {
+    public init() {}
+    public func test() {
+        print("Hi from Testing!")
+    }
+}
